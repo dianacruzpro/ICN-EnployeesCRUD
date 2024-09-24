e8321f2304d370c581ea88ab0d1aa3ceb66cda33
dianacruzpro <saravcruz1501@gmail.com>
2024-09-23 19:51:25 -0600

    Necessary tools and project creation

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ICN - Project</title>
+
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./styles.css">
+  <link rel="stylesheet" href="./pluging/sweetAlert2/sweetalert2.min.css">
+</head>
+<body>
+  
+
+  <script src="./jquery/jquery-3.7.1.min.js"></script>
+  <script src="./bootstrap/js/bootstrap.min.js"></script>
+  <script src="./popper/popper.min.js"></script>
+
+  <script src="pluging/sweetAlert2/sweetalert2.all.min.js"></script>
+  <script src="app.js"></script>
+</body>
+</html>
